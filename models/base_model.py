6b5ca34b8ca29a3d480147bb9c5f164cc5ffa143
Nii-101 <noelamptey101@gmail.com>
2023-12-09 21:18:45 +0000

    made some changes

--- models/base_model.py
+++ models/base_model.py
@@ -24,26 +24,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """ Should Print:[<class name>]
        (<self.id>)
        <self.__dict__>
        """
-        classname = type(self).__name__
+        classname = self.__class__.__name__
         return f"[{classname}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns all keys/values of __dict__ of the instance"""
         key_dict = self.__dict__.copy()
-
-        key_dict['__class__'] = type(self).__name__
+        key_dict['__class__'] = self.__class__.__name__
         key_dict['created_at'] = self.created_at.isoformat()
         key_dict['updated_at'] = self.updated_at.isoformat()
 
