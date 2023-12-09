65a69694c93320207e03fe613fc12acc21f250d3
Nii-101 <noelamptey101@gmail.com>
2023-12-09 11:11:16 +0000

    Finally done with task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""A class BaseModel"""
+"""Main class that serves as base for other models"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Should Print:[<class name>]
@@ -35,6 +37,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns all keys/values of __dict__ of the instance"""
