c4ba3a3dd0c8d14efffaf6dddc3095fa734007d9
Nii-101 <noelamptey101@gmail.com>
2023-12-09 21:31:47 +0000

    checking requirement succes

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """Main class that serves as base for other models"""
+
+
 import uuid
 from datetime import datetime
 from models import storage
@@ -17,7 +20,7 @@ class BaseModel:
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    if key != __class__:
+                    if key != "__class__":
                         self.__dict__[key] = value
 
         else:
