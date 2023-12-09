d0c84090c4f162bcc9ee69d1e2c1b14947b2227e
Nii-101 <noelamptey101@gmail.com>
2023-12-09 21:20:23 +0000

    resolved error

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """ Should Print:[<class name>]
@@ -37,7 +37,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Returns all keys/values of __dict__ of the instance"""
