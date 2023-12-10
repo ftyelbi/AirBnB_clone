38cc9bf81e577238a07bbf60b7ebd55fb4131c48
Nii-101 <noelamptey101@gmail.com>
2023-12-10 22:33:59 +0000

    Checking changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import json
-import os
 
 class FileStorage:
     __file_path = "storage.json"
@@ -44,11 +43,11 @@ class FileStorage:
     def reload(self):
         """Deserializes JSON file to instances and stores in __objects"""
         try:
-            if os.path.exists(FileStorage.__file_path):
-                with open(FileStorage.__file_path, 'r') as file:
-                    data = json.load(file)
-                    data = key: self.classes()[value["__class__"]](**value)
-                           for key, value in data.items()}
-                    FileStorage.__objects = data
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                data = {key: self.classes()[value["__class__"]](**value)
+                        for key, value in data.items()}
+                FileStorage.__objects = data
         except FileNotFoundError:
             pass
+
